40e9abea334ff74eb309eee06f1f84525672f0c1
PiringerMark <Piringerm@kkszki.hu>
2026-09-09 10:12:42 +0200

    mysql added

---
+++ BlogAPI/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using BlogAPI.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<blogger> GetAllBloggers() {
+            return null;
+        }
+        [HttpPost]
+        public void AddNewBlogger(blogger blogger)
+        {
+            return null;
+        }
+        [HttpPut]
+        public object updateBlogger(int id, blogger blogger)
+        {
+            return null;
+        }
+        [HttpDelete]
+        public object deleteBlogger(int id) {
+
+            return null;
+        }
+    }
+}
